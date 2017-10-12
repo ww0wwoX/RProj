aeb7a758df2b331290f0b8018a4ea17cb310ec3b
ww0wwoX <ww0wwoX@mail.ru>
2017-10-12 22:32:22 +0300

    create db

---
+++ RProject/RProject.Db/Install/001_CreateDb.sql
@@ -0,0 +1,14 @@
+﻿DECLARE @dbname NVARCHAR(80);
+SET @dbname = N'RPojectDB';
+
+IF (EXISTS (
+	SELECT name
+	FROM master.dbo.sysdatabases
+	WHERE ('[' + name + ']' = @dbname OR name = @dbname)))
+BEGIN
+	PRINT N'Database ' + @dbname + ' already exists.';
+END
+ELSE
+BEGIN
+	EXECUTE('CREATE DATABASE ' + @dbname);
+END
