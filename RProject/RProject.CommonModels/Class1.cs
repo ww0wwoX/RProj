f7dcb0207b37ad1dbea4a995cce09b8c1908f985
ww0wwpx <ww0wwox@mail.ru>
2017-10-12 22:07:13 +0300

    security & common

---
+++ RProject/RProject.CommonModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RProject.CommonModels
+{
+	public class Class1
+	{
+	}
+}
---
+++ RProject/RProject.Security/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RProject.Security
+{
+	public class Class1
+	{
+	}
+}
