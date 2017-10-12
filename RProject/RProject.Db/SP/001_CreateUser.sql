be9ba216596c7bf55a6f670523e03413ab31cf2a
ww0wwoX <ww0wwoX@mail.ru>
2017-10-13 00:16:21 +0300

    sql for create user

---
+++ RProject/RProject.Db/SP/001_CreateUser.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [Rights].[CreateUser]
+	@login NVARCHAR(50),
+	@firstName NVARCHAR(32),
+	@lastName NVARCHAR(32),
+	@hpword NVARCHAR(128),
+	@roles INT = 1
+AS
+BEGIN
+	INSERT
+	INTO [Rights].[Users] (
+		[Login]
+		, [FirstName]
+		, [LastName]
+		, [HPWord]
+		, [Roles]
+	) VALUES (
+		@login
+		, @firstName
+		, @lastName
+		, @hpword
+		, @roles
+	)
+END
